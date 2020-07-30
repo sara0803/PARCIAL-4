da63e122c62236944ce8249f485336ef47ccb10a
sara0803 <62775463+sara0803@users.noreply.github.com>
2020-07-30 09:25:07 -0500

    PRIMER AVANCE

---
+++ objeto.cpp
@@ -0,0 +1,93 @@
+#include "objeto.h"
+
+int Objeto::getAceleraciony() const
+{
+    return aceleraciony;
+}
+
+void Objeto::setAceleraciony(int value)
+{
+    aceleraciony = value;
+}
+
+Objeto::Objeto()
+{
+
+}
+
+Objeto::Objeto(int x, int y, int r, int acely, QString rt)
+{
+    posx=x;
+    posy=y;
+    radio=r;
+    ruta=rt;
+    aceleraciony=acely;
+    setPos(posx,posy);
+}
+
+int Objeto::getR()
+{
+    return radio;
+}
+
+void Objeto::setR(int r)
+{
+    radio=r;
+}
+
+
+int Objeto::getPosx()
+{
+    return posx;
+}
+
+void Objeto::setPosx(int  px)
+{
+    posx=px;
+}
+
+int Objeto::getPosy()
+{
+    return posy;
+}
+
+void Objeto::setPosy(int py)
+{
+    posy = py;
+}
+void Objeto::subir()
+{
+     setPos(x(),y()-5);
+}
+
+void Objeto::bajar()
+{
+    setPos(x(),y()+5);
+}
+
+void Objeto::derecha()
+{
+    setPos(x()+5,y());
+}
+
+void Objeto::izquierda()
+{
+    setPos(x()-5,y());
+}
+
+QRectF Objeto::boundingRect() const
+{
+     return QRectF(-radio,-radio,2*radio,2*radio);
+
+}
+
+void Objeto::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+  /*  QPixmap pixmap;
+
+    pixmap.load(ruta);
+
+    painter->drawPixmap(boundingRect(),pixmap,pixmap.rect());*/
+    painter->setBrush(Qt::blue);
+    painter->drawEllipse(boundingRect());
+}
---
+++ objeto.h
@@ -0,0 +1,33 @@
+#ifndef OBJETO_H
+#define OBJETO_H
+
+#include <QGraphicsItem>
+#include <QPainter>
+class Objeto  : public QGraphicsItem
+{
+
+    int radio;
+    int posx, posy;
+    int aceleraciony;
+    QString ruta;
+
+public:
+    Objeto();
+    Objeto(int x, int y, int r, int acely, QString ruta);
+    int getR();
+    void setR(int r);
+    int getPosx();
+    void setPosx(int px);
+    int getPosy();
+    void setPosy(int py);
+    void subir();
+    void bajar();
+    void derecha();
+    void izquierda();
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+    int getAceleraciony() const;
+    void setAceleraciony(int value);
+};
+
+#endif // OBJETO_H
